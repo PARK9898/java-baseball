b6901ea8669e17382e3d4d7b774d8d0476f6d07b
PARK9898 <wjddjs21@naver.com>
2023-06-05 16:28:51 +0900

    Number -> Numbers class 명 변경 PlayerNum -> PlayerNumbers 정적팩토리 메소드명 변경 Checkvalidate -> checkvalidate 함수명 코드 컨벤션 준수

--- src/main/java/baseball/Domain/Numbers.java
+++ src/main/java/baseball/Domain/Numbers.java
@@ -6,16 +6,16 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 
 
-public class Number {
+public class Numbers {
 	private static NumberValidator numberValidator = new NumberValidator();
 	private List<Integer> numbers = new ArrayList<>();
 
 
-	private Number(List<Integer> numbers) {
+	private Numbers(List<Integer> numbers) {
 		this.numbers = numbers;
 	}
 
-	public static Number ComputerNum(){
+	public static Numbers ComputerNumbers(){
 
 		List<Integer> ComNumbers = new ArrayList<>();
 		while(ComNumbers.size() < 3){
@@ -25,22 +25,22 @@ public class Number {
 			}
 		}
 
-		CheckValidate(ComNumbers);
+		checkValidate(ComNumbers);
 
-		return new Number(ComNumbers);
+		return new Numbers(ComNumbers);
 	}
 
-	public static Number PlayerNum(List<Integer> playerNum){
-		CheckValidate(playerNum);
+	public static Numbers PlayerNumbers(List<Integer> playerNum){
+		checkValidate(playerNum);
 
-		return new Number(playerNum);
+		return new Numbers(playerNum);
 	}
 
 	public List<Integer> getNumbers() {
 		return numbers;
 	}
 
-	private static void CheckValidate(List<Integer> numbers){
+	private static void checkValidate(List<Integer> numbers){
 		numberValidator.checkSize(numbers);
 		numberValidator.checkDuplicate(numbers);
 		numberValidator.checkRange(numbers);
